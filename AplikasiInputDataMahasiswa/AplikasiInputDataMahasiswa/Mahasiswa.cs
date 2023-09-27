4bbc01b8a5823530317f78ad4a7dc922a5db2c4f
JeCoas <127827888+JeCoas@users.noreply.github.com>
2023-09-27 12:15:44 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    class Mahasiswa
+    {
+        public string NIM;
+        public string Nama;
+        public string Kelas;
+        public int Nilai;
+        public string Huruf;
+    }
+}
